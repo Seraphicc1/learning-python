6195468462b2a6033a06bb0f74c5a457a20ca091
Seraphicc1 <seyhan.pc2@gmail.com>
2025-12-18 13:27:29 +0100

    Create 01_print.py

---
+++ 01_print.py
@@ -0,0 +1,5 @@
+# To make a comment in Python you need to use a "#"
+## Comments will not be shown when you run the program
+
+print("I am new to programming.")
+print("I am currently learning the basics.")
