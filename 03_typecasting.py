ef2f7ada9e67edfdc44c9356fdc972d6de3cc32e
Seraphicc1 <seyhan.pc2@gmail.com>
2025-12-18 17:17:50 +0100

    First steps with datatypes
    - typecasting
    - type-function
    - user input

---
+++ 03_typecasting.py
@@ -0,0 +1,14 @@
+# Useful for user input
+
+name = "Seraphicc"
+age = 22
+height = 1.73
+is_developer = True
+
+age = str(age)
+height = int(height)
+name = bool(name)
+is_developer = float(is_developer)
+
+print(type(age))
+print(is_developer)
---
+++ 04_user_input.py
@@ -0,0 +1,8 @@
+preference = input("What do you like?: ")
+age = int(input("How old are you?: "))
+
+age = age + 1
+
+print(f"Oh great! I love {preference}!")
+print(f"{age} is not too late to become the greatest!")
+print("Oh btw, HAPPY BIRTHDAY!")
