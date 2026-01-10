20b4f52f3581e49bf2fa481e412704f2cb6d2fa6
Seraphicc1 <seyhan.pc2@gmail.com>
2026-01-10 09:13:06 +0100

    New Progress
    learnt about string methods and indexing, next time I will learn about format specifiers, now work starts again after the holiday and I will learn every monday - friday if possible while at work

---
+++ 10_string_methods.py
@@ -0,0 +1,14 @@
+# print(help(str))
+
+name = input("Enter you name: ")
+
+len(name)
+name.find("a")
+name.rfind("e")
+name.capitalize()
+name.upper()
+name.lower()
+name.isdigit()
+name.isalpha()
+name.count("a")
+name.replace("a", " ")
---
+++ 11_string_indexing.py
@@ -0,0 +1,9 @@
+credit_number = "1243-214-1244-111"
+
+credit_number[3]
+credit_number[:6]
+credit_number[5:9]
+credit_number[7:]
+credit_number[-4]
+credit_number[::3]
+credit_number[::-1]
